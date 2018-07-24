38565feaf7688aea421ef20e32e745da061c3156
PAT <marijuana.w420z@gmail.com>
2018-07-24 11:36:38 +0700

    version 1

---
+++ Github/src/RunPerson.java
@@ -0,0 +1,11 @@
+
+public class RunPerson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p = new Person("Pattarachon "," Chominjak");
+		System.out.println("Name :"+p.getName()+p.getLastname());
+
+	}
+
+}
